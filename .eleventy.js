86fb59084c401b7a8bbb06556c5703f58a2385bf
Damian <58891112+damianroiz@users.noreply.github.com>
2021-11-14 11:09:33 -0500

    setting up 11ty
    frontmatter data is not working correnclty

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
 
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/index.njk');
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin');
@@ -9,6 +10,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
+  
 
   return {
     dir: {
