a52e348e7d38cbe080794ff3a75cfb877f0d886c
Damian <58891112+damianroiz@users.noreply.github.com>
2021-11-17 21:10:39 -0500

    parsed frontmatter data -resolved
    changes to njk and .json files to resolve the issue with parsed data

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,6 @@
-const { DateTime } = require("luxon");
-
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/index.njk');
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
-  eleventyConfig.addPassthroughCopy('./src/admin');
-
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
-
-  
 
   return {
     dir: {
@@ -20,3 +9,4 @@ module.exports = function(eleventyConfig) {
     }
   };
 }
+
